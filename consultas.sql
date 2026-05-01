4b4362fe33a0e2567e709aa7e7f103ec0120388c
carolcarvalho-27 <carolinacmana@gmail.com>
2026-05-01 16:53:13 -0300

    Update consultas.sql
    atualizações em nomes de atributos/entidades

--- consultas.sql
+++ consultas.sql
@@ -2,111 +2,111 @@
 
 -- cadastro de usuário
 
-INSERT INTO usuario (nome, email, senha, status)
+INSERT INTO user (name, email, password, status)
 VALUES ('João Silva', 'jsilva@ufmg.com', 'joao_silva03', 'ativo');
 
 --exclusão (desativar) conta
 
-DELETE FROM usuario
-WHERE id_usuario = 1;
+DELETE FROM user
+WHERE id_user = 1;
 
-UPDATE usuario
+UPDATE user
 SET status = 'inativo'
-WHERE id_usuario = 1;
+WHERE id_user = 1;
 
 --suspender conta
 
-UPDATE usuario
+UPDATE user
 SET status = 'suspenso'
-WHERE id_usuario = 1;
+WHERE id_user = 1;
 
 --cadastrar obras e exemplares
 
-INSERT INTO obra (titulo, genero, tipo, ano, edicao, autor)
+INSERT INTO work (title, genre, type, year, edition, author)
 VALUES ('Engenharia de Software Moderna', 'Tecnologia', 'Livro', 2020, 2, 'Marco Tulio Valente');
 
-INSERT INTO exemplar (id_obra, status)
+INSERT INTO copy (id_work, status)
 VALUES (1, 'disponivel'), (2, 'disponivel'), (3, 'disponivel');
 
 --deletar obras
 
-DELETE FROM obra
-WHERE id_obra = 1;
+DELETE FROM work
+WHERE id_work = 1;
 
 --pesquisar obras:
 
 --por título:
 
-SELECT * FROM obra
-WHERE titulo ILIKE '%casmurro%';
+SELECT * FROM work
+WHERE title ILIKE '%casmurro%';
 
 --por autor
-SELECT * FROM obra
-WHERE autor ILIKE '%machado%';
+SELECT * FROM work
+WHERE author ILIKE '%machado%';
 
 --por disponibilidade
-SELECT o.titulo, COUNT(e.id_exemplar) AS disponiveis
-FROM obra o
-JOIN exemplar e ON o.id_obra = e.id_obra
-WHERE e.status = 'disponivel'
-GROUP BY o.titulo;
+SELECT w.title, COUNT(c.id_copy) AS disponiveis
+FROM work w
+JOIN copy c ON w.id_work = c.id_work
+WHERE c.status = 'disponivel'
+GROUP BY w.titulo;
 
 --pegar livro emprestado
 
-INSERT INTO emprestimo (
-    id_usuario,
-    id_exemplar,
-    data_emprestimo,
-    data_prevista_devolucao,
+INSERT INTO loan (
+    id_user,
+    id_copy,
+    date_loan,
+    date_return_expected,
     status
 )
 VALUES (1, 3, '2026-06-20', '2026-06-20' + INTERVAL '15 days', 'ativo');
 
-UPDATE exemplar
+UPDATE copy
 SET status = 'emprestado'
-WHERE id_exemplar = 3;
+WHERE id_copy = 3;
 
 --devolver livro
 
-UPDATE emprestimo
-SET data_devolucao = CURRENT_DATE,
+UPDATE loan
+SET date_return = CURRENT_DATE,
     status = 'devolvido'
-WHERE id_emprestimo = 1;
+WHERE id_loan = 1;
 
-UPDATE exemplar
+UPDATE copy
 SET status = 'disponivel'
-WHERE id_exemplar = 3;
+WHERE id_copy = 3;
 
 --visualizar empréstimos
 
 SELECT 
-    u.nome,
-    o.titulo,
-    e.data_emprestimo,
-    e.data_prevista_devolucao
-FROM emprestimo e
-JOIN usuario u ON e.id_usuario = u.id_usuario
-JOIN exemplar ex ON e.id_exemplar = ex.id_exemplar
-JOIN obra o ON ex.id_obra = o.id_obra
-WHERE e.status = 'ativo'
-AND u.id_usuario = 1;
+    u.name,
+    w.title,
+    l.date_loan,
+    l.date_return_expected
+FROM loan l
+JOIN user u ON l.id_user = u.id_user
+JOIN copy c ON l.id_copy = c.id_copy
+JOIN work w ON c.id_work = w.id_work
+WHERE l.status = 'ativo'
+AND u.id_user = 1;
 
 --consultar atrasos
 
 SELECT 
-    u.nome,
-    o.titulo,
-    e.data_prevista_devolucao
-FROM emprestimo e
-JOIN usuario u ON e.id_usuario = u.id_usuario
-JOIN exemplar ex ON e.id_exemplar = ex.id_exemplar
-JOIN obra o ON ex.id_obra = o.id_obra
-WHERE e.status = 'ativo'
-AND e.data_prevista_devolucao < CURRENT_DATE;
+    u.name,
+    w.title,
+    l.date_return_expected
+FROM loan l
+JOIN user u ON l.id_user = u.id_user
+JOIN copy c ON l.id_copy = c.id_copy
+JOIN work w ON c.id_work = w.id_work
+WHERE l.status = 'ativo'
+AND l.date_return_expected < CURRENT_DATE;
 
 --criar reserva
 
-INSERT INTO reserva (id_usuario, id_obra, status)
+INSERT INTO reservation (id_user, id_work, status)
 VALUES (1, 2, 'ativa');
 
 
